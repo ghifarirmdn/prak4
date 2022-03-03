41b173c80ce5e0819da32e1acb5c9a6414eec103
Ghifari Ramadhan <ghifariramadhan2001@gmail.com>
2022-03-03 21:58:27 +0700

    percobaan 2

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class DetailScreen extends StatelessWidget{
     return Scaffold(
       body: SafeArea(
         child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Container(
                 margin: const EdgeInsets.only(top: 16.0),
@@ -37,7 +38,15 @@ class DetailScreen extends StatelessWidget{
                   ),
                 ),
               ),
-            ]
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: const Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel odio vitae ex bibendum cursus. Vestibulum aliquet posuere sapien egestas iaculis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc quis mauris molestie nibh vulputate iaculis et ac justo. Nam tempus egestas massa, ut mollis diam vestibulum vel. Integer id interdum tortor, vel interdum turpis. In ultrices ornare erat, egestas interdum neque varius vel.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              )
+            ],
         ),
     ),
     );
