3151a33e3b8a4ef24a356d2308857132651a3172
Ghifari Ramadhan <ghifariramadhan2001@gmail.com>
2022-03-03 22:19:38 +0700

    percobaan 3

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,20 @@ class DetailScreen extends StatelessWidget{
                 ),
               ),
               Container(
+                margin: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: const <Widget> [
+                        Icon(Icons.calendar_today),
+                        Text("Open Everyday"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
                 padding: const EdgeInsets.all(16.0),
                 child: const Text(
                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel odio vitae ex bibendum cursus. Vestibulum aliquet posuere sapien egestas iaculis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc quis mauris molestie nibh vulputate iaculis et ac justo. Nam tempus egestas massa, ut mollis diam vestibulum vel. Integer id interdum tortor, vel interdum turpis. In ultrices ornare erat, egestas interdum neque varius vel.",
